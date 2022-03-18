476cfbd797db4e81c0bb79841cdc971ce8047755
Eder Santos <3derms@gmail.com>
2022-03-18 18:20:01 -0300

    feat: Cria função para saber o horario de maior acesso

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,16 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    count = 0
+    previous_count = 0
+    for element in permanence_period:
+        if (
+            target_time == "" or
+            type(element[0]) is not int or
+            type(element[1]) is not int
+            ):
+            return None
+        if element[0] <= target_time <= element[1]:
+            count = count + 1
+        if count > previous_count:
+            previous_count = count
+    return previous_count
+
