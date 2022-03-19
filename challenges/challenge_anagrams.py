18c7e1ece71136e5a88da82c39b9f453dd8d71f5
Eder Santos <3derms@gmail.com>
2022-03-19 17:24:44 -0300

    feat: Cria funçao que verifica se e anagrama

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,28 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if(
+        len(first_string) != len(second_string) or
+        first_string == "" or second_string == ""
+        ):
+        return False
+
+    f_string = list(first_string.casefold())
+    s_string = list(second_string.casefold())
+
+    has_swapped = True
+    num_of_iterations = 0
+
+    while has_swapped:
+        has_swapped = False
+        for letter in range(len(s_string) - num_of_iterations - 1):
+            if f_string[letter] != s_string[letter]:
+                f_string[letter], f_string[letter + 1] = f_string[letter + 1], f_string[letter]
+                has_swapped = True
+        num_of_iterations += 1
+
+    f_string = "".join(f_string)
+    s_string = "".join(s_string)
+
+    if (f_string != s_string):
+        return False
+
+    return True
--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -6,6 +6,6 @@ def is_palindrome_recursive(word, low_index, high_index):
         return False
     if (low_index == high_index):
         return True
-    if (low_index <= high_index) :
+    if (low_index <= high_index):
         return is_palindrome_recursive(word, low_index + 1, high_index - 1)
     return True
