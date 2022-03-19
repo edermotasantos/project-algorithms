69446b6d9cc03b1aca33dd290e2e183457ae30d6
Eder Santos <3derms@gmail.com>
2022-03-19 20:55:43 -0300

    feat: cria funçao que verifica se e palindromo de forma iterativa

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,11 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    if (word == ""):
+        return False
+    high_index = len(word) - 1
+
+    for index in range(len(word) // 2):
+        if (word[index] != word[high_index]):
+            return False
+        high_index = high_index - 1
+        index = index + 1
+    return True
