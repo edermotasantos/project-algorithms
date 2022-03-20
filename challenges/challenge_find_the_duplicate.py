373e931993d7354a3f1c5bbac58f064f732ef607
Eder Santos <3derms@gmail.com>
2022-03-19 21:13:14 -0300

    refactor: refatora funçao que encontra numero duplicado

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,27 +1,19 @@
-from array import array
-import string
-
-
-def find_duplicate(nums):
+def found_duplicate(nums, duplicate):
     count = 0
-    duplicate = 0
-    if (
-        nums == [] or
-        type(nums[0]) == str or
-        len(nums) == 0 or
-        nums[0] < 0
-        ):
-        return False
-
     for i in range(len(nums)):
 
         for j in range(i + 1, len(nums)):
-            if (
-                nums[j] == nums[i] and
-                i != j
-                ):
-                    duplicate = nums[j]
-                    count = count + 1
+            if (nums[j] == nums[i] and i != j):
+                duplicate = nums[j]
+                count = count + 1
     if count == 0:
         return False
     return duplicate
+
+
+def find_duplicate(nums):
+    duplicate = 0
+    if (nums == [] or type(nums[0]) == str or len(nums) == 0 or nums[0] < 0):
+        return False
+
+    return found_duplicate(nums, duplicate)
