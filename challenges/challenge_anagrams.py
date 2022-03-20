9150b3e813f4aff2f8c7614de7d54a2edca50f03
Eder Santos <3derms@gmail.com>
2022-03-19 21:46:53 -0300

    fix: Corrige lint em challenge_anagrams

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,28 +1,32 @@
-def is_anagram(first_string, second_string):
-    if (
-        len(first_string) != len(second_string) or
-        first_string == "" or second_string == ""
-        ):
-        return False
-
-    f_string = list(first_string.casefold())
-    s_string = list(second_string.casefold())
-
+def check_string(f_str, s_str):
     has_swapped = True
     num_of_iterations = 0
 
     while has_swapped:
         has_swapped = False
-        for letter in range(len(s_string) - num_of_iterations - 1):
-            if f_string[letter] != s_string[letter]:
-                f_string[letter], f_string[letter + 1] = f_string[letter + 1], f_string[letter]
+        for letr in range(len(s_str) - num_of_iterations - 1):
+            if f_str[letr] != s_str[letr]:
+                f_str[letr], f_str[letr + 1] = f_str[letr + 1], f_str[letr]
                 has_swapped = True
         num_of_iterations += 1
 
-    f_string = "".join(f_string)
-    s_string = "".join(s_string)
+    f_str = "".join(f_str)
+    s_str = "".join(s_str)
 
-    if (f_string != s_string):
+    if (f_str != s_str):
         return False
 
     return True
+
+
+def is_anagram(first_string, second_string):
+    if (
+        len(first_string) != len(second_string) or
+        first_string == "" or second_string == ""
+    ):
+        return False
+
+    f_str = list(first_string.casefold())
+    s_str = list(second_string.casefold())
+
+    return check_string(f_str, s_str)
