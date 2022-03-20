cac5157608243edfdfa333d9e7583d9fd5a08401
Eder Santos <3derms@gmail.com>
2022-03-19 21:24:38 -0300

    fix: corrige lint de challenge_study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -2,12 +2,11 @@ def study_schedule(permanence_period, target_time):
     count = 0
     for element in permanence_period:
         if (
-            target_time == None or
-            type(element[0]) is not int or
-            type(element[1]) is not int
-            ):
+            target_time is None or
+            type(element[0]) != int or
+            type(element[1]) != int
+        ):
             return None
         if element[0] <= target_time <= element[1]:
             count = count + 1
     return count
-
