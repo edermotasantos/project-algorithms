9ee7de2e404a58365ac00b8f3368a86d3cf25762
Eder Santos <3derms@gmail.com>
2022-03-21 12:05:18 -0300

    refactor: diminui tempo de execuçao do challenge_palindromes_interative

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,11 +1,8 @@
 def is_palindrome_iterative(word):
     if (word == ""):
         return False
-    high_index = len(word) - 1
 
     for index in range(len(word) // 2):
-        if (word[index] != word[high_index]):
+        if (word[index] != word[len(word) - 1 - index]):
             return False
-        high_index = high_index - 1
-        index = index + 1
     return True
