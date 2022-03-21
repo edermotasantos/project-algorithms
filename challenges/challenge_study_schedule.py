238f8a996b97479214ffc910f6cca3aa0594ff4a
Eder Santos <3derms@gmail.com>
2022-03-21 11:44:58 -0300

    Refactor: diminui tempo

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,8 +1,9 @@
 def study_schedule(permanence_period, target_time):
     count = 0
+    if(target_time is None):
+        return None
     for element in permanence_period:
         if (
-            target_time is None or
             type(element[0]) != int or
             type(element[1]) != int
         ):
